c1c4b98d5659f46ecf03dcd3828c7e5b7a0b241d
Greg Turnquist <gturnquist@gopivotal.com>
2013-05-09 10:48:07 -0500

    Trimmed out the UserService domain, and instead just have it print a simple message.

--- complete/src/main/java/schedulingtasks/CleanOutUnactivatedAccounts.java
+++ complete/src/main/java/schedulingtasks/CleanOutUnactivatedAccounts.java
@@ -1,9 +1,5 @@
 package schedulingtasks;
 
-import java.util.Date;
-import java.util.Iterator;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,19 +8,8 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 public class CleanOutUnactivatedAccounts {
 	
-	@Autowired
-	public UserService userService;
-	
 	@Scheduled(fixedRate=5000)
-    public void lookForOldAccountsAndDeleteThem() {
-        System.out.println("Checking for old accounts");
-        Iterator<String> keys = userService.users.keySet().iterator();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            if (new Date().getTime() - userService.users.get(key).getTime() > 30000) {
-                System.out.println("User " + key + " is over 30 seconds old. Deleting.");
-                keys.remove();
-            }
-        }
+    public void greetingsFromScheduledSpring() {
+		System.out.println("Greetings from a Spring-scheduled task!");
     }
 }
