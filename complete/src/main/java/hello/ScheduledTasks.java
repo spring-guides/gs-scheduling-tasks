0ee70c84c45b79b8da1fd57f9df7265714bfde79
Chris Beams <cbeams@gopivotal.com>
2013-05-22 13:20:21 +0200

    Edit scheduled task 'complete' sources
     - Use SpringApplication#run in #main
     - Refactor ScheduledTask for better method naming and output
    
    These edits have been made only to 'complete' sources, and need to be
    reflected in the README as well.
    
    [#48116733]

--- complete/src/main/java/hello/ScheduledTasks.java
+++ complete/src/main/java/hello/ScheduledTasks.java
@@ -1,15 +1,17 @@
 package hello;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
-@Component
 @EnableScheduling
 public class ScheduledTasks {
-	
-	@Scheduled(fixedRate=5000)
-    public void greetingsFromScheduledSpring() {
-		System.out.println("Greetings from a Spring-scheduled task!");
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
     }
 }
