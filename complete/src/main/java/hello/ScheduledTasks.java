d60eab65369cc49e1a030691d09a013ce701cc23
Dave Syer <dsyer@pivotal.io>
2016-08-25 12:22:41 +0100

    Add a simple integration test

--- complete/src/main/java/hello/ScheduledTasks.java
+++ complete/src/main/java/hello/ScheduledTasks.java
@@ -3,16 +3,20 @@ package hello;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTasks {
 
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
     public void reportCurrentTime() {
-        System.out.println("The time is now " + dateFormat.format(new Date()));
+        log.info("The time is now {}", dateFormat.format(new Date()));
     }
 }
