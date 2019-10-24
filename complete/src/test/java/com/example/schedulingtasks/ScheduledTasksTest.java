79ac4e457e1b3945f320870b8d09daa68208e989
Jay Bryant <jbryant@pivotal.io>
2019-10-24 15:30:14 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to work with JUnit 5 and replacing the Gradle wrappers.

--- complete/src/test/java/com/example/schedulingtasks/ScheduledTasksTest.java
+++ complete/src/test/java/com/example/schedulingtasks/ScheduledTasksTest.java
@@ -17,19 +17,15 @@
 package com.example.schedulingtasks;
 
 import org.awaitility.Duration;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.verify;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ScheduledTasksTest {
 
--- complete/src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTest.java
+++ complete/src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTest.java
@@ -18,13 +18,11 @@ package com.example.schedulingtasks;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SchedulingTasksApplicationTest {
 
